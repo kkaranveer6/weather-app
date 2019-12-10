8181ce0f1259b0395fb1c7eceb55e3be7a259882
Karanveer <karanveer@Karanveers-MacBook-Pro.local>
2019-12-10 11:14:57 +1030

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-    </div>
-  );
+
+	const API_KEY = `91a00bed4607b32dc90c7ff92b4cddbc`;
+
+	const onButtonSubmit = async () => {
+		const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
+		const data = await response.json();
+		console.log(data);
+	}
+
+	return (
+	<div className="App">
+	  <h1>Weather App</h1>
+	  <input name="City Search" type="text" placeholder="Enter City"/>
+	  <button 
+	  	onClick = {() => onButtonSubmit()}>
+	  	Search
+	  </button>
+	</div>
+	);
 }
 
 export default App;
