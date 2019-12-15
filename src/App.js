576559690b601b21eb1cdcbb2ed48882c82a28ac
Karanveer <karanveer@Karanveers-MacBook-Pro.local>
2019-12-15 16:42:12 +1030

    creating search bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App(){
-
 	const API_KEY = `91a00bed4607b32dc90c7ff92b4cddbc`;
 
-	const onButtonSubmit = () => {
-	// fetching data from an API
-		const fetchData = fetch(`http://api.openweathermap.org/data/2.5/weather?q=Adelaide,aus&APPID=${API_KEY}&units=metric`)
-							.then(response => response.json())
-							.then(data => console.log(data))
-							.catch(err => console.log('Something went wrong'))
+	// state
+	const [query, setQuery] = useState('');
+	const [data, setData] = useState('');
+
+	// runs when search button is clicked
+	const getData = () => {
+		// fetching data from an API
+			const response = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query},aus&APPID=${API_KEY}&units=metric`)
+								.then(response => response.json())
+								// .then(data => console.log(data))
+								.catch(err => console.log('Something went wrong'))
+			setData(response);
+			console.log(data);
+		}
+	
+	const updateSearch = (event) => {
+		setQuery(event.target.value);
+		console.log(event.target.value);
 	}
 
 	return (
-	<div className="App">
-	  <h1>Weather App</h1>
-	  <input name="City Search" type="text" placeholder="Enter City"/>
-	  <button 
-	  	onClick = {() => onButtonSubmit()}>
-	  	Search
-	  </button>
-	</div>
+		<div className="App">
+		  <h1>Weather App</h1>
+		  <input 
+		  	name = "City Search" 
+		  	type = "text" 
+		  	placeholder = "Enter City"
+		  	onChange = {updateSearch}
+ 		  />
+		  <button 
+		  	onClick = {getData}
+		  >
+		  	Search
+		  </button>
+		</div>
 	);
 }
+
+
 
 export default App;
